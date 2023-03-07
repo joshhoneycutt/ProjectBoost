d239a3d5d57c5abeabd04a5964b566669bd14300
Joshua Honeycutt <joshdhh@gmail.com>
2023-03-06 22:33:59 -0500

    Videos 42 - 44

---
+++ Assets/Scripts/CollsionHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CollsionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is Friendly");
+                break;
+            case "Finish":
+                Debug.Log("This thing is Finish");
+                break;
+            case "Fuel":
+                Debug.Log("this thing is fuel");
+                break;
+            default:
+                Debug.Log("Sorry, you blew up");
+                break;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
